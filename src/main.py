81085c24f77c482c1d4b51e2e2f8205eac971f17
luizfernando <luiz-fsilva37@hotmail.com>
2020-02-17 17:20:37 -0300

    Atualização main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from backtracking import permutacao
 
-print(f'Exemplo 1: {permutacao(1, ["a", "b", "c", "d"])}')
-print(f'Exemplo 2: {permutacao(2, ["a", "b", "c", "d"])}')
+print(f'Exemplo 1: {permutacao(1, ["a", "b", "c"])}')
+
+print(f'Exemplo 2: {permutacao(2, ["a", "b", "c"])}')
+
+print(f'Exemplo 3: {permutacao(3, ["a", "b", "c"])}')
